5784c9dd6e1726cb99f64c426ed72b72cf94f370
Kanon Kakuno <yadex205@outlook.jp>
2019-02-24 11:13:36 +0900

    :new: Introduce concurrently to run multi webpack simultaneously.

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,9 @@ const app = express();
 app.use('/', express.static(resolve(__dirname, './public')));
 
 app.get('/api', (req, res) => {
-  res.send(resolve(__dirname, './public'));
+  res.json({
+    version: '0.0.0'
+  });
 });
 
 app.listen(DEFAULT_PORT);
